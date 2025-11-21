e0be4aa7fc334aa95f49deb6c627cc281d19604b
Osif ungar <osif.e.u@gmail.com>
2025-11-21 17:27:02 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-#!/usr/bin/env bash
-set -e
-
-export PORT="${PORT:-8000}"
-
-echo "Starting SLH_Wallet_2.0 on port ${PORT} ..."
-uvicorn app.main:app --host 0.0.0.0 --port "${PORT}"
+#!/bin/bash
+uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
